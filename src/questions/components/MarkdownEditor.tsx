7784b588b285e1d0299a214fea6eba9cdea9387a
Donghee Nam <donghee.nam04@gmail.com>
2022-10-30 17:53:47 +1100

    Complete design for question editor

--- src/questions/components/MarkdownEditor.tsx
+++ src/questions/components/MarkdownEditor.tsx
@@ -1,9 +1,11 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 
 const MarkdownParser = dynamic(() => import("./MarkdownParser"), {
   suspense: true,
 });
+
+import styles from "./MarkdownEditor.module.scss";
 
 export function MarkdownEditor({
   text,
@@ -12,13 +14,27 @@ export function MarkdownEditor({
   text: string;
   setText: (value: string) => void;
 }) {
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const nRows = textRef.current?.value
+    ? textRef.current.value.split("\n").length + 1
+    : 2;
+
   return (
-    <div>
-      <textarea value={text} onChange={(e) => setText(e.target.value)} />
-      <div>
-        <Suspense fallback={text}>
-          <MarkdownParser text={text} />
-        </Suspense>
+    <div className={styles.root}>
+      <div className={styles.control}>Buttons</div>
+      <div className={styles.editor}>
+        <textarea
+          ref={textRef}
+          className={styles.input}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          rows={nRows}
+        />
+        <div className={styles.preview}>
+          <Suspense fallback={text}>
+            <MarkdownParser text={text} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
